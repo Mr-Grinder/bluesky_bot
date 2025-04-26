dd176bb0589b6c4a603dd675dcb4fa98c49a355b
Mr-Grinder <aaa3122556@gmail.com>
2025-04-26 17:37:55 +0300

    git add git commit -m 'Fixed code' git push

--- bot.py
+++ bot.py
@@ -3,20 +3,14 @@ import os
 
 bot = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
 
-ALLOWED_USER_ID = 1066936794  
+ALLOWED_USER_ID = 1066936794
 
-@bot.message_handler(func=lambda message: True)
+def is_allowed(message):
+    return message.from_user.id == ALLOWED_USER_ID
+
+@bot.message_handler(func=is_allowed)
 def handle_message(message):
-    user_id = message.from_user.id
-    username = message.from_user.username
-    first_name = message.from_user.first_name
     text = message.text.strip()
-    
-    print(f"Повідомлення від {first_name} (@{username}), ID: {user_id} — {text}")
-
-    if user_id != ALLOWED_USER_ID:
-        return  #Просто ігноруємо 
-
     bot.send_message(message.chat.id, f"Прийнято! Твій текст:\n{text}")
 
 bot.polling()
